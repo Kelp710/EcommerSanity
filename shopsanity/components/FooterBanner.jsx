c9f04f515a8e2c2160d9d9bfee8c8f1f4a749bea
Harumi Yamashita <bz1786354@gmail.com>
2022-12-12 13:46:41 -0500

    add footer banner

--- shopsanity/components/FooterBanner.jsx
+++ shopsanity/components/FooterBanner.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import Link from 'next/link';
+import { urlFor } from '../lib/Client';
 
-const FooterBanner = () => {
+const FooterBanner = ({footerBanner : {discount, largeText1, largeText2, saleTime, smallText, midText, desc, product, buttonText, image}}) => {
   return (
-    <div>FooterBanner!</div>
+    <div className='footer-banner-container'>
+      <div className='banner-desc'>
+        <div className='left'>
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <h3>{saleTime}</h3>
+        </div>
+        <div className='right'>
+          <p>{smallText}</p>
+          <p>{midText}</p>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type='button'>
+              {buttonText}
+            </button>
+          </Link>
+        </div>
+        {console.log(image.asset)}
+        <img 
+        src={urlFor(image)}
+        className="footer-banner-image"
+        />
+      </div>
+    </div>
   )
 }
 
--- shopsanity/pages/index.js
+++ shopsanity/pages/index.js
@@ -14,8 +14,9 @@ const Home = ({ products, bannerData }) => {
       </div>
       <div className='products-container'>
        { products?.map((product)=> <Product key={product._id} product={product}/>)}
+       {console.log(products)}
       </div>
-      <FooterBanner/>
+      <FooterBanner footerBanner={bannerData && bannerData[0]}/>
     </>
   )
 }
