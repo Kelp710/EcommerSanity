7cd01fc00b5159f3cc73326d76fb75569d0afbb0
Harumi Yamashita <bz1786354@gmail.com>
2022-12-13 17:31:03 -0500

    added navbar

--- shopsanity/components/FooterBanner.jsx
+++ shopsanity/components/FooterBanner.jsx
@@ -10,11 +10,11 @@ const FooterBanner = ({footerBanner : {discount, largeText1, largeText2, saleTim
           <p>{discount}</p>
           <h3>{largeText1}</h3>
           <h3>{largeText2}</h3>
-          <h3>{saleTime}</h3>
+          <p>{saleTime}</p>
         </div>
         <div className='right'>
           <p>{smallText}</p>
-          <p>{midText}</p>
+          <h3>{midText}</h3>
           <p>{desc}</p>
           <Link href={`/product/${product}`}>
             <button type='button'>
